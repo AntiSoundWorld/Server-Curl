c43b7151b235cd3299dae0e93b70485b7fd389e9
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-07 00:09:13 +0300

    test

--- Server-Curl.c
+++ Server-Curl.c
@@ -5,6 +5,23 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef struct List
+{
+    char* bodyEmptyTask;
+    char* status;
+    char* type;
+    char* length;
+    char* connection;
+    char* lineBreak;
+    char* responseId;
+    char* responseName;
+    char* space;
+    char* openedBracket;
+    char* closedBracket;
+    char* quotes;
+    char* colon;
+    char* comma;
+}list_t;
 typedef struct Parse
 {
     char* request;
@@ -31,6 +48,8 @@ char* BuildResponse(int id, char* names);
 void SendResponse(int clientSocket, char* request);
 void Update(int clientSocket, task_t* head, parse_t* ParseRequest);
 task_t* Delete(int socketClient, task_t* head, parse_t* parseRequest);
+list_t* List();
+void FreeList(list_t* list);
 
 int main()
 {
@@ -268,59 +287,46 @@ task_t* MethodInterractive(int socketClient, task_t* head, parse_t* parseRequest
 
 char* BuildResponse(int id, char* names)
 {
-    char bodyEmptyTask[] = "The task is empty";
-    size_t sizeOfEmptyTask = strlen(bodyEmptyTask);
-    
-    char status[] = "HTTP/1.1 200 OK \n";
-    size_t sizeOfStatus200 = strlen(status);
-
-    char type[] = "Content-Type: application/json \n";
-    size_t sizeOfType = strlen(type);
-
-    char length[] = "Content-Length: ";
-    size_t sizeOfLength = strlen(length);
-
-    char connection[] = "Connection: keep-alive \n";
-    size_t sizeOfConnection = strlen(connection);
-
-    char lineBreak[] = "\n";
-    size_t sizeOfLineBreak = strlen(lineBreak);
+    list_t* list = List();
 
     if(id == 0)
     {
-        char* charSizeEmptyTask = calloc(sizeOfEmptyTask + 1, sizeof(char));
-        sprintf(charSizeEmptyTask, "%ld", sizeOfEmptyTask);
+        char* charSizeEmptyTask = calloc(strlen(list->bodyEmptyTask) + 1, sizeof(char));
+        sprintf(charSizeEmptyTask, "%ld", strlen(list->bodyEmptyTask));
 
-        char* ContentLength = calloc(sizeOfLength + sizeOfEmptyTask + sizeOfLineBreak + 1, sizeof(char)); 
-        strcat(ContentLength, length);
-        strcat(ContentLength, charSizeEmptyTask);
-        strcat(ContentLength, lineBreak);
+        char* contentLength = calloc(strlen(list->length) + strlen(list->bodyEmptyTask) + strlen(list->lineBreak) + 1, sizeof(char)); 
+        strcat(contentLength, list->length);
+        strcat(contentLength, charSizeEmptyTask);
+        strcat(contentLength, list->lineBreak);
         
-        size_t sizeOfrequest = sizeOfStatus200 + sizeOfType + sizeOfLength + sizeOfConnection + sizeOfLineBreak + sizeOfEmptyTask;
+        size_t sizeOfrequest = strlen(list->status) + strlen(list->type) + strlen(contentLength) + strlen(list->connection) 
+        + strlen(list->lineBreak) + strlen(list->bodyEmptyTask);
+
         char* buildedResponse = calloc(sizeOfrequest + 1, sizeof(char));
 
-        strcat(buildedResponse, status);
-        strcat(buildedResponse, type);
-        strcat(buildedResponse, ContentLength);
-        strcat(buildedResponse, connection);
-        strcat(buildedResponse, lineBreak);
-        strcat(buildedResponse, bodyEmptyTask);
+        strcat(buildedResponse, list->status);
+        strcat(buildedResponse, list->type);
+        strcat(buildedResponse, contentLength);
+        strcat(buildedResponse, list->connection);
+        strcat(buildedResponse, list->lineBreak);
+        strcat(buildedResponse, list->bodyEmptyTask);
 
         //printf("%s \n", buildedResponse); //show full response
         free(charSizeEmptyTask);
-        free(ContentLength);
-
+        free(contentLength);
+        free(list);
         return buildedResponse;
     }
     if(id == 1)
     {
-        size_t sizeOfrequest = sizeOfStatus200 + sizeOfConnection + sizeOfLineBreak + 1;
+        size_t sizeOfrequest = strlen(list->status) + strlen(list->connection) + strlen(list->lineBreak) + 1;
         char* buildedResponse = calloc(sizeOfrequest + 1, sizeof(char));
 
-        strcat(buildedResponse, status);
-        strcat(buildedResponse, connection);
-        strcat(buildedResponse, lineBreak);
+        strcat(buildedResponse, list->status);
+        strcat(buildedResponse, list->connection);
+        strcat(buildedResponse, list->lineBreak);
 
+        free(list);
         //printf("\nFull response\n%s \n", buildedResponse); //show full response
         return buildedResponse;
     }
@@ -331,21 +337,25 @@ char* BuildResponse(int id, char* names)
         char* charSizeOfNames = calloc(sizeOfNames + 1, sizeof(char));
         sprintf(charSizeOfNames, "%ld", sizeOfNames);
 
-        char* ContentLength = calloc(sizeOfLength + sizeOfNames + sizeOfLineBreak + 1, sizeof(char));
-        strcat(ContentLength, length);
+        char* ContentLength = calloc(strlen(list->length) + sizeOfNames + strlen(list->lineBreak) + 1, sizeof(char));
+        strcat(ContentLength, list->length);
         strcat(ContentLength, charSizeOfNames);
-        strcat(ContentLength, lineBreak);
+        strcat(ContentLength, list->lineBreak);
         size_t sizeOfContentLength = strlen(ContentLength);
 
-        size_t sizeOfResponse = sizeOfStatus200 + sizeOfType + sizeOfContentLength + sizeOfConnection + sizeOfLineBreak + sizeOfNames + 1;
+        size_t sizeOfResponse = strlen(list->status) + strlen(list->type) + sizeOfContentLength + strlen(list->connection) 
+        + strlen(list->lineBreak) + sizeOfNames + 1;
+
         char* buildedResponse = calloc(sizeOfResponse + 1, sizeof(char));
-       
-        strcat(buildedResponse, status);
-        strcat(buildedResponse, type);
+
+        strcat(buildedResponse, list->status);
+        strcat(buildedResponse, list->type);
         strcat(buildedResponse, ContentLength);
-        strcat(buildedResponse, connection);
-        strcat(buildedResponse, lineBreak);
+        strcat(buildedResponse, list->connection);
+        strcat(buildedResponse, list->lineBreak);
         strcat(buildedResponse, names);
+
+        FreeList(list);
 
         return buildedResponse;
     }
@@ -373,79 +383,52 @@ void SendResponse(int clientSocket, char* buildedResponse)
 
 char* BuildNames(char* buildedNames, task_t* pointer)
 {
+    list_t* list = List();
+
     int id = pointer->id;
+    char* charId = calloc(id + 1, sizeof(char));
+    sprintf(charId, "%d", id);
 
     char* name = pointer->name;
     size_t sizeOfName = strlen(name);
 
-    char responseId[] = "Id";
-    size_t sizeOfRersosnseId = strlen(responseId);
+    char* fullResponseId = calloc(strlen(list->quotes) + strlen(list->responseId) + strlen(list->quotes) + strlen(list->colon) 
+    + strlen(list->space) + strlen(list->quotes) + strlen(charId) + strlen(list->quotes) + 1, sizeof(char));
 
-    char responseName[] = "name";
-    size_t sizeOfRersosnseName = strlen(responseName);
-
-    char breakLine[] = "\n";
-    size_t sizeOfBreakLine = strlen(breakLine);
-
-    char space[] = " ";
-    size_t sizeOfSpace = strlen(space);
-
-    char* charId = calloc(id + 1, sizeof(char));
-    sprintf(charId, "%d", id);
-    size_t sizeOfCharId = strlen(charId);
-
-    char openedBracket[] = "{";
-    size_t sizeOfOpenedBracket = strlen(openedBracket);
-
-    char closedBracket[] = "}";
-    size_t sizeOfClosedBracket = strlen(closedBracket);
-
-    char quotes[] = "\"";
-    size_t sizeOfQuotes = strlen(quotes);
-
-    char colon[] = ":";
-    size_t sizeOfColon = strlen(colon);
-
-    char comma[] = ",";
-    size_t sizeOfComma = strlen(comma);
-    
-
-    char* fullResponseId = calloc(sizeOfQuotes + sizeOfRersosnseId + sizeOfQuotes + sizeOfColon + sizeOfSpace + sizeOfQuotes + sizeOfCharId + sizeOfQuotes + 1, sizeof(char));
-
-    strcat(fullResponseId, quotes);
-    strcat(fullResponseId, responseId);
-    strcat(fullResponseId, quotes);
-    strcat(fullResponseId, colon);
-    strcat(fullResponseId, space);
-    strcat(fullResponseId, quotes);
+    strcat(fullResponseId, list->quotes);
+    strcat(fullResponseId, list->responseId);
+    strcat(fullResponseId, list->quotes);
+    strcat(fullResponseId, list->colon);
+    strcat(fullResponseId, list->space);
+    strcat(fullResponseId, list->quotes);
     strcat(fullResponseId, charId);
-    strcat(fullResponseId, quotes);
+    strcat(fullResponseId, list->quotes);
     
-    size_t sizeOfFullResponseId = strlen(fullResponseId);
 
-    char* fullResponseName = calloc(sizeOfQuotes + sizeOfRersosnseName + sizeOfQuotes + sizeOfColon + sizeOfSpace + sizeOfComma + sizeOfName + sizeOfComma + 1, sizeof(char));
+    char* fullResponseName = calloc(strlen(list->quotes) + strlen(list->responseName) + strlen(list->quotes) + strlen(list->colon) 
+    + strlen(list->space) + strlen(list->comma) + sizeOfName + strlen(list->comma) + 1, sizeof(char));
 
-    strcat(fullResponseName, quotes);
-    strcat(fullResponseName, responseName);
-    strcat(fullResponseName, quotes);
-    strcat(fullResponseName, colon);
-    strcat(fullResponseName, space);
-    strcat(fullResponseName, quotes);
+    strcat(fullResponseName, list->quotes);
+    strcat(fullResponseName, list->responseName);
+    strcat(fullResponseName, list->quotes);
+    strcat(fullResponseName, list->colon);
+    strcat(fullResponseName, list->space);
+    strcat(fullResponseName, list->quotes);
     strcat(fullResponseName, name);
-    strcat(fullResponseName, quotes);
+    strcat(fullResponseName, list->quotes);
     
-    size_t sizeOfFullResponseName = strlen(fullResponseName);
-
     if(buildedNames == NULL)
     {
-        size_t sizeOfBuiledNames = sizeOfOpenedBracket + sizeOfFullResponseId + sizeOfFullResponseName + sizeOfClosedBracket + sizeOfBreakLine + 1;
+        size_t sizeOfBuiledNames = strlen(list->openedBracket) + strlen(fullResponseId) + strlen(fullResponseName) 
+        + strlen(list->closedBracket) + strlen(list->lineBreak) + 1;
+
         buildedNames = calloc(sizeOfBuiledNames, sizeof(char));
     }
     else
     {
-        size_t sizeOfBuildedNames = strlen(buildedNames);
+        size_t sizeOfBufferBuildedNames = strlen(buildedNames) + strlen(list->openedBracket) + strlen(fullResponseId) + strlen(list->comma) 
+        + strlen(list->space) + strlen(fullResponseName) + strlen(list->closedBracket) + strlen(list->lineBreak) + 1;
 
-        size_t sizeOfBufferBuildedNames = sizeOfBuildedNames + sizeOfOpenedBracket + sizeOfFullResponseId + sizeOfComma  + sizeOfSpace + sizeOfFullResponseName + sizeOfClosedBracket + sizeOfBreakLine + 1;
         char* bufferBuildedNames = calloc(sizeOfBufferBuildedNames + 1, sizeof(char));
         strcat(bufferBuildedNames, buildedNames);
 
@@ -453,18 +436,19 @@ char* BuildNames(char* buildedNames, task_t* pointer)
 
         buildedNames = bufferBuildedNames;
     }
-    strcat(buildedNames, openedBracket);
+    strcat(buildedNames, list->openedBracket);
     strcat(buildedNames, fullResponseId);
-    strcat(buildedNames, comma);
-    strcat(buildedNames, space);
+    strcat(buildedNames, list->comma);
+    strcat(buildedNames, list->space);
     strcat(buildedNames, fullResponseName);
-    strcat(buildedNames, closedBracket);
-    strcat(buildedNames, breakLine);
+    strcat(buildedNames, list->closedBracket);
+    strcat(buildedNames, list->lineBreak);
     
     free(charId);
     free(fullResponseId);
     free(fullResponseName);
-    
+    FreeList(list);
+
     return buildedNames;
 }
 
@@ -550,6 +534,7 @@ void Update(int socketClient, task_t* head, parse_t* parseRequest)
 
     SendResponse(socketClient, BuildResponse(1, NULL));
 }
+
 task_t* Delete(int socketClient, task_t* head, parse_t* parseRequest)
 {
     int id = ParseId(parseRequest);
@@ -605,4 +590,86 @@ task_t* Delete(int socketClient, task_t* head, parse_t* parseRequest)
     }
 
     return head;
+}
+
+list_t* List()
+{
+    list_t* list = malloc(sizeof(list_t));
+
+    char bodyEmptyTask[] = "The task is empty";
+    list->bodyEmptyTask = calloc(strlen(bodyEmptyTask) + 1, sizeof(char));
+    strcpy(list->bodyEmptyTask, bodyEmptyTask);
+
+    char status[] = "HTTP/1.1 200 OK \n";
+    list->status = calloc(strlen(status) + 1, sizeof(char));
+    strcpy(list->status, status);
+
+    char type[] = "Content-Type: application/json \n";
+    list->type = calloc(strlen(type) + 1, sizeof(char));
+    strcpy(list->type, type);
+
+    char length[] = "Content-Length: ";
+    list->length = calloc(strlen(length) + 1, sizeof(char));
+    strcpy(list->length, length);
+
+    char connection[] = "Connection: keep-alive \n";
+    list->connection = calloc(strlen(connection) + 1, sizeof(char));
+    strcpy(list->connection, connection);
+
+    char lineBreak[] = "\n";
+    list->lineBreak = calloc(strlen(lineBreak) + 1, sizeof(char));
+    strcpy(list->lineBreak, lineBreak);
+
+    char responseId[] = "Id";
+    list->responseId = calloc(strlen(responseId) + 1, sizeof(char));
+    strcpy(list->responseId, responseId);
+
+    char responseName[] = "name";
+    list->responseName = calloc(strlen(responseName) + 1, sizeof(char));;
+    strcpy(list->responseName, responseName);
+
+    char space[] = " ";
+    list->space = calloc(strlen(space) + 1, sizeof(char));;
+    strcpy(list->space, space);
+
+    char openedBracket[] = "{";
+    list->openedBracket = calloc(strlen(openedBracket) + 1, sizeof(char));;
+    strcpy(list->openedBracket, openedBracket);
+
+    char closedBracket[] = "}";
+    list->closedBracket = calloc(strlen(closedBracket) + 1, sizeof(char));;;
+    strcpy(list->closedBracket, closedBracket);
+
+    char quotes[] = "\"";
+    list->quotes = calloc(strlen(quotes) + 1, sizeof(char));;
+    strcpy(list->quotes, quotes);
+
+    char colon[] = ":";
+    list->colon = calloc(strlen(colon) + 1, sizeof(char));;
+    strcpy(list->colon, colon);
+
+    char comma[] = ",";
+    list->comma = calloc(strlen(comma) + 1, sizeof(char));;
+    strcpy(list->comma, comma);
+
+    return list;
+}
+
+void FreeList(list_t* list)
+{
+    free(list->bodyEmptyTask);
+    free(list->status);
+    free(list->type);
+    free(list->length);
+    free(list->connection);
+    free(list->lineBreak);
+    free(list->responseId);
+    free(list->responseName);
+    free(list->space);
+    free(list->openedBracket);
+    free(list->closedBracket);
+    free(list->quotes);
+    free(list->colon);
+    free(list->comma);
+    free(list);
 }
