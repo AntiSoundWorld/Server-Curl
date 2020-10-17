249af8c4ea7e425d7b56b374e681e7f30da9a0c6
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-17 11:54:41 +0300

    check errors Url

--- Request.sh
+++ Request.sh
@@ -1,7 +1,7 @@
 curl 127.0.0.1:8090
 
-curl 127.0.0.1:8090 -d '{"name":"Dmitry"}' 
+curl 127.0.0.1:8090 -H Content-Type: application/json -d '{"name":"Dmitry"}' 
 
 curl -v 127.0.0.1:8090 -X DELETE -H Content-Type: application/json -d '{"id":"0"}'
 
-curl 127.0.0.1:8090 -X PUT -d '{"Id":"0","name":"Anastasia"}'
+curl 127.0.0.1:8090 -X PUT -H Content-Type: application/json -d '{"Id":"0","name":"Anastasia"}'
